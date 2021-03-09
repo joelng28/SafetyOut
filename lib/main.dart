dcd7667c652cde98ba3eda32e354d443d11d7133
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2021-03-09 16:17:24 +0100

    Primer commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:app/routes/router.dart';
+import 'package:app/state/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AuthState(),
+      child: MyApp()
+    )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SafetyOU',
+      themeMode: ThemeMode.system, //Utilitza el tema del sistema
+      // Més endevant podem controlar amb una variable si l'usuari ho canvia des de l'aplicació
+      theme: ThemeData(),
+      darkTheme: ThemeData(),
+      
+      onGenerateRoute: AppRouter.generateRoute,
+      initialRoute: '/',
+    );
+  }
+}
---
+++ lib/routes/router.dart
@@ -0,0 +1,27 @@
+import 'package:app/pages/home.dart';
+import 'package:app/pages/login.dart';
+import 'package:app/state/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AppRouter {
+  static const String home = '/';
+  static const String login = '/login';
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case home:
+        return MaterialPageRoute(
+          settings: settings,
+          builder: (context) => Provider.of<AuthState>(context).logInState ? Home() : Login()
+        );
+      case login:
+        return MaterialPageRoute(
+          settings: settings,
+          builder: (context) => Login()
+        );
+      default:
+        return null;
+    }
+  }
+}
