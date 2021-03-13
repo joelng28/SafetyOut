0a2c07234a602b68b7698892b83be8b80313f583
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2021-03-13 20:37:50 +0100

    Base internacionalitzacio

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:app/app_localizations.dart';
 import 'package:app/routes/router.dart';
 import 'package:app/state/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -13,11 +15,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SafetyOU',
+      title: 'SafetyOUT',
       themeMode: ThemeMode.system, //Uses default system theme
       // Més endevant podem controlar amb una variable si l'usuari ho canvia des de l'aplicació
       theme: ThemeData(),
       darkTheme: ThemeData(),
+
+      supportedLocales: [
+        Locale('en', ''),
+        Locale('cat', ''),
+        Locale('es', ''),
+      ],
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
 
       onGenerateRoute: AppRouter.generateRoute,
       initialRoute: '/',
