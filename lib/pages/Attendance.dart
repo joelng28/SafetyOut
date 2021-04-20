685178609e128278e3a631e15270cbb0f141572a
Sergi Muro <sergi.muro@estudiantat.upc.edu>
2021-04-20 17:27:22 +0200

    Diseño mejorado para el uso de APIs

--- lib/pages/Attendance.dart
+++ lib/pages/Attendance.dart
@@ -1,9 +1,27 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
 class Attendance {
   String user_id;
   String latitud;
   String longitud;
   DateTime fecha;
   int duracion;
+
+  String get getPlace{  
+    return null;
+  }
+
+  DateTime get getFechaInicial => fecha;
+  DateTime get getFechaFinal => fecha;
+
+  void setFechaInicial(DateTime f){
+    fecha = f;
+  }
+
+  void setFechaFinal(DateTime f){
+    fecha = f;
+  }
 
 
 }
--- lib/pages/DetailsAttendance.dart
+++ lib/pages/DetailsAttendance.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_picker/flutter_picker.dart';
+import 'package:table_calendar/table_calendar.dart';
 import '../app_localizations.dart';
 import '../defaults/constants.dart';
 import '../pages/Attendance.dart';
@@ -15,9 +17,19 @@ class DetailsAttendance extends StatefulWidget {
 }
 
 class _DetailsAttendanceState extends State<DetailsAttendance> {
-  Function guardarCanvis = (BuildContext context) {};
+  Function guardarCanvis = (BuildContext context) {
+    //CRIDA API
+    Navigator.of(context).pop();
+  };
 
-    Function borrarAssistencia = (BuildContext context) {};
+  Function borrarAssistencia = (BuildContext context) {
+    //CRIDA API
+    Navigator.of(context).pop();
+  };
+
+  var _calendarController;
+
+  var pickedDate;
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,39 +95,146 @@ class _DetailsAttendanceState extends State<DetailsAttendance> {
             Padding(
               padding: EdgeInsets.fromLTRB(Constants.h4(context),
                   Constants.v7(context), Constants.h4(context), 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Visibility(
-                      visible: MediaQuery.of(context).viewInsets.bottom == 0,
-                      child: Text(
-                          widget.assistencia.placename,
-                          style: TextStyle(
-                              color: Constants.black(context),
-                              fontSize: Constants.xxl(context),
-                              fontWeight: Constants.bolder)),
-                  ),
-                  Text(
-                      widget.assistencia.placeLocation,
-                      style: TextStyle(
-                          color: Constants.black(context))
-                  ),
-                  Text(
-                      widget.assistencia.date + widget.assistencia.startHour + 
-                      " - " + widget.assistencia.endHour,
-                      style: TextStyle(
-                          color: Constants.black(context))
-                  ),
-                  Text(
-                      widget.assistencia.personas,
-                      style: TextStyle(
-                          color: Constants.black(context))
-                  ),
-                  
-                ],
-              ),
+              child: Text(
+                  widget.assistencia.latitud, //placename,
+                  style: TextStyle(
+                      color: Constants.black(context),
+                      fontSize: Constants.xxl(context),
+                      fontWeight: Constants.bolder)),
             ),
-            Expanded(
+            Text(
+              widget.assistencia.longitud,//placeLocation,
+              style: TextStyle(
+                  color: Constants.black(context))
+            ),
+            TableCalendar( //DatePicker
+                  locale:
+                     Localizations.localeOf(context).toLanguageTag(),
+                          startDay: DateTime(1900),
+                          endDay: DateTime.now(),
+                          calendarController: _calendarController,
+                          initialSelectedDay: pickedDate,
+                          initialCalendarFormat: CalendarFormat.month,
+                          availableCalendarFormats: const {
+                            CalendarFormat.month: '',
+                          },
+                          availableGestures: AvailableGestures.all,
+                          calendarStyle: CalendarStyle(
+                            outsideDaysVisible: false,
+                            weekendStyle: TextStyle(
+                                color: Constants.black(context),
+                                fontSize: Constants.m(context)),
+                            holidayStyle: TextStyle(
+                                color: Constants.black(context),
+                                fontSize: Constants.m(context)),
+                            weekdayStyle: TextStyle(
+                                color: Constants.black(context),
+                                fontSize: Constants.m(context)),
+                            selectedColor: Constants.primary(context),
+                            selectedStyle: TextStyle(color: Color(0xFF242424)),
+                            todayStyle:
+                                TextStyle(color: Constants.black(context)),
+                            todayColor: Colors.transparent,
+                          ),
+                          daysOfWeekStyle: DaysOfWeekStyle(
+                            weekdayStyle: TextStyle(
+                                color: Constants.darkGrey(context),
+                                fontSize: Constants.m(context),
+                                fontWeight: Constants.bold),
+                            weekendStyle: TextStyle(
+                                color: Constants.darkGrey(context),
+                                fontSize: Constants.m(context),
+                                fontWeight: Constants.bold),
+                          ),
+                          headerStyle: HeaderStyle(
+                            centerHeaderTitle: true,
+                            formatButtonVisible: false,
+                            titleTextStyle: TextStyle(
+                                fontWeight: Constants.bold,
+                                fontSize: Constants.l(context)),
+                            leftChevronIcon: Icon(Icons.arrow_back_ios_rounded,
+                                color: Constants.black(context)),
+                            rightChevronIcon: Icon(
+                                Icons.arrow_forward_ios_rounded,
+                                color: Constants.black(context)),
+                          ),
+                          onDaySelected: (date, events, holidays) =>
+                              setState(() {
+                            pickedDate = date;
+                            _calendarController.setSelectedDay(pickedDate);
+                          }),
+                          onHeaderTapped: (date) {
+                            pickedDate = date;
+                            _calendarController.setSelectedDay(pickedDate);
+                            Picker(
+                              adapter: DateTimePickerAdapter(
+                                  type: 11,
+                                  yearBegin: 1900,
+                                  yearEnd: DateTime.now().year,
+                                  value: pickedDate,
+                                  months: [
+                                    AppLocalizations.of(context)
+                                        .translate("Gener"),
+                                    AppLocalizations.of(context)
+                                        .translate("Febrer"),
+                                    AppLocalizations.of(context)
+                                        .translate("Març"),
+                                    AppLocalizations.of(context)
+                                        .translate("Abril"),
+                                    AppLocalizations.of(context)
+                                        .translate("Maig"),
+                                    AppLocalizations.of(context)
+                                        .translate("Juny"),
+                                    AppLocalizations.of(context)
+                                        .translate("Juliol"),
+                                    AppLocalizations.of(context)
+                                        .translate("Agost"),
+                                    AppLocalizations.of(context)
+                                        .translate("Setembre"),
+                                    AppLocalizations.of(context)
+                                        .translate("Octubre"),
+                                    AppLocalizations.of(context)
+                                        .translate("Novembre"),
+                                    AppLocalizations.of(context)
+                                        .translate("Desembre"),
+                                  ],
+                                  customColumnType: [
+                                    0,
+                                    1
+                                  ]),
+                              textStyle: TextStyle(
+                                  color: Constants.darkGrey(context),
+                                  fontSize: Constants.l(context)),
+                              columnFlex: [2, 3],
+                              confirmText: AppLocalizations.of(context)
+                                  .translate("Confirmar"),
+                              cancelText: AppLocalizations.of(context)
+                                  .translate("Cancel·lar"),
+                              confirmTextStyle: TextStyle(
+                                  color: Constants.darkGrey(context),
+                                  fontWeight: Constants.bold,
+                                  fontSize: Constants.s(context)),
+                              cancelTextStyle: TextStyle(
+                                  color: Constants.darkGrey(context),
+                                  fontWeight: Constants.bold,
+                                  fontSize: Constants.s(context)),
+                              height: Constants.a11(context),
+                              hideHeader: true,
+                              selectedTextStyle: TextStyle(
+                                color: Constants.black(context),
+                              ),
+                              backgroundColor: Constants.lightGrey(context),
+                              onConfirm: (Picker picker, List values) =>
+                                  setState(() {
+                                pickedDate = DateTime(1900 + values[0],
+                                    values[1] + 1, pickedDate.day);
+                                _calendarController.setSelectedDay(pickedDate);
+                              }),
+                              onCancel: () {},
+                            ).showDialog(context);
+                          },
+                        ),
+            Expanded( //BotoBorrarAsistencia
               child: SizedBox(
                 height: Constants.a7(context),
                 child: Container(
