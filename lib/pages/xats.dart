afe8789c04195a0398ec13a4ef4b75b6182a0489
Joel <joelnavarrogil@gmail.com>
2021-05-09 23:16:18 +0200

    Inici sincrontzació BackEnd

--- lib/pages/xats.dart
+++ lib/pages/xats.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_socket_io/flutter_socket_io.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class Chats extends StatefulWidget {
   Chats({Key key}) : super(key: key);
@@ -9,6 +11,18 @@ class Chats extends StatefulWidget {
 }
 
 class _Chats extends State<Chats> {
+  @override
+  void initState() {
+    super.initState();
+    IO.Socket socket = IO.io('https://safetyout.herokuapp.com/');
+    socket.connect();
+    socket.emit('join', {
+      'user1_id': '6081a40d875b4b3864bd1f21',
+      'user2_id': '609116e842fa750022ab15b7'
+    });
+    //socket.emit('message', {'chatRoom': })
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
