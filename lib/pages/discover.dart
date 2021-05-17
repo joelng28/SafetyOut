5fbaec5e915f466550b3c5bcfcf146f983f41765
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2021-05-17 22:26:46 +0200

    Release 3

--- lib/pages/discover.dart
+++ lib/pages/discover.dart
@@ -267,12 +267,12 @@ class _Discover extends State<Discover> {
     places = [];
     await Future.wait([
       http.get(urlPark),
-/*       http.get(urlNature),
+      http.get(urlNature),
       http.get(urlSight),
       http.get(urlTerrace),
       http.get(urlMountain),
       http.get(urlCastle),
-      http.get(urlRes) */
+      http.get(urlRes)
     ]).then((List responses) {
       responses.forEach((r) {
         Map<String, dynamic> body = jsonDecode(r.body);
